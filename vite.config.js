9c8a46c289897edefe8f7708abb17a12c0b6b9ab
SanjayKumar <0508sanjaykumar@gmail.com>
2024-06-30 15:49:51 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Todo-List/',
+  base: '/Todo-List/',  // Ensure this matches your GitHub repository name
 })
